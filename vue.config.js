4c06c19d0f133fdfca60b896130dd1bd2a620a62
duytanle <leduytan1905@gmail.com>
2022-08-30 00:42:33 +0700

    adding dist

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,5 @@
 const { defineConfig } = require("@vue/cli-service");
 module.exports = defineConfig({
   transpileDependencies: true,
-  publicPath:
-    process.env.NODE_ENV === "production" ? "/pokemon-memories-game/" : "/",
+  publicPath: "/pokemon-memories-game/",
 });
